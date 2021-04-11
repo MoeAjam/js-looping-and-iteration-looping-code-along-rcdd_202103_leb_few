3012d899d7e431cee75c4406f3c2096c61d224b2
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-11 15:05:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function writeCards(myArray[], eventName){
+function writeCards(myArray, eventName){
   let result;
   for(let i=0; i<myArray.length;i++){
-
+    result[i]= "Thank you, ${myArray[i]}, for the"
   }
 }
