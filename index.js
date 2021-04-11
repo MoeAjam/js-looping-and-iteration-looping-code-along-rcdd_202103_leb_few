f8c8b06ec305ec3fbb4c834539ca22e867229666
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-11 15:14:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// function writeCards(myArray, eventName){
-//   let result;
-//   for(let i=0; i<myArray.length;i++){
-//     result[i]= "Thank you, ${myArray[i]}, for the wonderful ${eventName} gift!";
-//   }
-//   return result;
-// }
+function writeCards(myArray, eventName){
+  let result;
+  for(let i=0; i<myArray.length;i++){
+    result[i]= `Thank you, ${myArray[i]}, for the wonderful ${eventName} gift!``;
+  }
+  return result;
+}
 // writeCards(["Ada", "Brendan", "Ali"], "birthday");
 
 function countDown(n){
