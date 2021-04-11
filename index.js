f6119f904cce12cc375146509cd7b943e2660da1
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-11 15:07:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function writeCards(myArray, eventName){
-  let result;
+  const result;
   for(let i=0; i<myArray.length;i++){
     result[i]= "Thank you, ${myArray[i]}, for the wonderful ${eventName} gift!";
   }
